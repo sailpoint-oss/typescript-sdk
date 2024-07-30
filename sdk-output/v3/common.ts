34b71a9ed24a0b250cf6363809982a3f4a424c32
darrell-thobe-sp <darrell-thobe-sp@users.noreply.github.com>
2024-07-30 15:04:42 +0000

    Bump version to 1.3.8

--- sdk-output/v3/common.ts
+++ sdk-output/v3/common.ts
@@ -135,9 +135,9 @@ export const createRequestFunction = function (axiosArgs: RequestArgs, globalAxi
     return <T = unknown, R = AxiosResponse<T>>(axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
         axiosRetry(globalAxios, configuration.retriesConfig)
         const headers = {
-            ...{'User-Agent':'OpenAPI-Generator/1.3.7/ts'}, 
+            ...{'User-Agent':'OpenAPI-Generator/1.3.8/ts'}, 
             ...axiosArgs.axiosOptions.headers,
-            ...{'X-SailPoint-SDK':'typescript-1.3.7'}
+            ...{'X-SailPoint-SDK':'typescript-1.3.8'}
         }
         axiosArgs.axiosOptions.headers = headers
         const axiosRequestArgs = {...axiosArgs.axiosOptions, url: (configuration?.basePath + "/v3" || basePath) + axiosArgs.url};
