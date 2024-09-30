65c497ff62a1c0269842eccebc36aa6dd44ebb90
darrell-thobe-sp <darrell.thobe@sailpoint.com>
2024-09-30 10:05:36 -0400

    updates for new generator

--- sdk-output/v3/api.ts
+++ sdk-output/v3/api.ts
@@ -13,7 +13,7 @@
  */
 
 
-import type { Configuration } from '../configuration';
+import type { Configuration } from './configuration';
 import type { AxiosPromise, AxiosInstance, RawAxiosRequestConfig } from 'axios';
 import globalAxios from 'axios';
 // Some imports not used depending on template conditions
