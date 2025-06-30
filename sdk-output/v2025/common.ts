c6b23f0b23f9dfdbc3b3da46b8b70b96b588e49a
darrell-thobe-sp <darrell-thobe-sp@users.noreply.github.com>
2025-06-30 14:04:58 +0000

    Bump version to v1.6.5

--- sdk-output/v2025/common.ts
+++ sdk-output/v2025/common.ts
@@ -147,9 +147,9 @@ export const createRequestFunction = function (axiosArgs: RequestArgs, globalAxi
     return <T = unknown, R = AxiosResponse<T>>(axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
         axiosRetry(axios, configuration.retriesConfig)
         const headers = {
-            ...{'User-Agent':'OpenAPI-Generator/1.6.4/ts'}, 
+            ...{'User-Agent':'OpenAPI-Generator/1.6.5/ts'}, 
             ...axiosArgs.axiosOptions.headers,
-            ...{'X-SailPoint-SDK':'typescript-1.6.4'}
+            ...{'X-SailPoint-SDK':'typescript-1.6.5'}
         }
 
         if(!configuration.experimental && ("X-SailPoint-Experimental" in headers)) {
