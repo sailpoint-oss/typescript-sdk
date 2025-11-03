8627dd09f644ae0bfc39c4728513ea07aeece838
tyler-mairose-sp <tyler-mairose-sp@users.noreply.github.com>
2025-11-03 14:59:31 +0000

    Bump version to v1.6.10

--- sdk-output/v2024/common.ts
+++ sdk-output/v2024/common.ts
@@ -147,9 +147,9 @@ export const createRequestFunction = function (axiosArgs: RequestArgs, globalAxi
     return <T = unknown, R = AxiosResponse<T>>(axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
         axiosRetry(axios, configuration.retriesConfig)
         const headers = {
-            ...{'User-Agent':'OpenAPI-Generator/1.6.9/ts'}, 
+            ...{'User-Agent':'OpenAPI-Generator/1.6.10/ts'}, 
             ...axiosArgs.axiosOptions.headers,
-            ...{'X-SailPoint-SDK':'typescript-1.6.9'}
+            ...{'X-SailPoint-SDK':'typescript-1.6.10'}
         }
 
         if(!configuration.experimental && ("X-SailPoint-Experimental" in headers)) {
