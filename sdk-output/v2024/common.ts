914e144d60a67157905ed070e971104f74285fab
developer-relations-sp <123671052+developer-relations-sp@users.noreply.github.com>
2026-02-17 20:27:02 +0000

    Bump version to v1.7.2

--- sdk-output/v2024/common.ts
+++ sdk-output/v2024/common.ts
@@ -147,9 +147,9 @@ export const createRequestFunction = function (axiosArgs: RequestArgs, globalAxi
     return <T = unknown, R = AxiosResponse<T>>(axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
         axiosRetry(axios, configuration.retriesConfig)
         const headers = {
-            ...{'User-Agent':'OpenAPI-Generator/1.7.1/ts'}, 
+            ...{'User-Agent':'OpenAPI-Generator/1.7.2/ts'}, 
             ...axiosArgs.axiosOptions.headers,
-            ...{'X-SailPoint-SDK':'typescript-1.7.1'}
+            ...{'X-SailPoint-SDK':'typescript-1.7.2'}
         }
 
         if(!configuration.experimental && ("X-SailPoint-Experimental" in headers)) {
