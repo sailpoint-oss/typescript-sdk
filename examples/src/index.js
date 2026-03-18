d77fa878c41458f51fffe6aa08b3c91a435bad09
Tyler Mairose <tyler.mairose@sailpoint.com>
2026-03-17 22:15:07 -0400

    Adding NERM to the typescript SDK

--- examples/src/index.js
+++ examples/src/index.js
@@ -35,6 +35,13 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var sailpoint_api_client_1 = require("sailpoint-api-client");
 var createTransform = function () { return __awaiter(void 0, void 0, void 0, function () {
@@ -155,4 +162,209 @@ var listEntitlements = function () { return __awaiter(void 0, void 0, void 0, fu
         }
     });
 }); };
-listEntitlements();
+// Generator Examples - Memory efficient pagination
+var getPaginatedAccountsWithGenerator = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var apiConfig, api, processedCount, _a, _b, _c, account, e_1_1;
+    var _d, e_1, _e, _f;
+    var _g;
+    return __generator(this, function (_h) {
+        switch (_h.label) {
+            case 0:
+                console.log("\n=== Using Generator for Accounts ===");
+                apiConfig = new sailpoint_api_client_1.Configuration();
+                api = new sailpoint_api_client_1.AccountsApi(apiConfig);
+                processedCount = 0;
+                _h.label = 1;
+            case 1:
+                _h.trys.push([1, 6, 7, 12]);
+                _a = true, _b = __asyncValues(sailpoint_api_client_1.Paginator.paginateGenerator(api, api.listAccounts, { limit: 100, sorters: "created" }, // Total limit of 100
+                20 // Page size of 20
+                ));
+                _h.label = 2;
+            case 2: return [4 /*yield*/, _b.next()];
+            case 3:
+                if (!(_c = _h.sent(), _d = _c.done, !_d)) return [3 /*break*/, 5];
+                _f = _c.value;
+                _a = false;
+                account = _f;
+                processedCount++;
+                console.log("Account ".concat(processedCount, ": ").concat(account.name, " (").concat(account.id, ")"));
+                // Example: Stop early if we find what we're looking for
+                if ((_g = account.name) === null || _g === void 0 ? void 0 : _g.includes("admin")) {
+                    console.log("Found admin account, stopping early");
+                    return [3 /*break*/, 5];
+                }
+                _h.label = 4;
+            case 4:
+                _a = true;
+                return [3 /*break*/, 2];
+            case 5: return [3 /*break*/, 12];
+            case 6:
+                e_1_1 = _h.sent();
+                e_1 = { error: e_1_1 };
+                return [3 /*break*/, 12];
+            case 7:
+                _h.trys.push([7, , 10, 11]);
+                if (!(!_a && !_d && (_e = _b.return))) return [3 /*break*/, 9];
+                return [4 /*yield*/, _e.call(_b)];
+            case 8:
+                _h.sent();
+                _h.label = 9;
+            case 9: return [3 /*break*/, 11];
+            case 10:
+                if (e_1) throw e_1.error;
+                return [7 /*endfinally*/];
+            case 11: return [7 /*endfinally*/];
+            case 12:
+                console.log("Total accounts processed: ".concat(processedCount));
+                return [2 /*return*/];
+        }
+    });
+}); };
+var searchWithGenerator = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var apiConfig, api, search, count, startTime, _a, _b, _c, doc, identity, e_2_1, elapsed;
+    var _d, e_2, _e, _f;
+    var _g;
+    return __generator(this, function (_h) {
+        switch (_h.label) {
+            case 0:
+                console.log("\n=== Using Generator for Search ===");
+                apiConfig = new sailpoint_api_client_1.Configuration();
+                api = new sailpoint_api_client_1.SearchApi(apiConfig);
+                search = {
+                    indices: ["identities"],
+                    query: { query: "*" },
+                    sort: ["name"] // Required for search pagination
+                };
+                count = 0;
+                startTime = Date.now();
+                _h.label = 1;
+            case 1:
+                _h.trys.push([1, 6, 7, 12]);
+                _a = true, _b = __asyncValues(sailpoint_api_client_1.Paginator.paginateSearchApiGenerator(api, search, 50, 200));
+                _h.label = 2;
+            case 2: return [4 /*yield*/, _b.next()];
+            case 3:
+                if (!(_c = _h.sent(), _d = _c.done, !_d)) return [3 /*break*/, 5];
+                _f = _c.value;
+                _a = false;
+                doc = _f;
+                identity = doc;
+                count++;
+                // Process each identity immediately
+                if (count <= 5) {
+                    console.log("Identity ".concat(count, ": ").concat(identity.name));
+                }
+                // Example use case: Find specific identities without loading all
+                if (((_g = identity.attributes) === null || _g === void 0 ? void 0 : _g.department) === "Engineering") {
+                    console.log("Found Engineering member: ".concat(identity.name));
+                }
+                _h.label = 4;
+            case 4:
+                _a = true;
+                return [3 /*break*/, 2];
+            case 5: return [3 /*break*/, 12];
+            case 6:
+                e_2_1 = _h.sent();
+                e_2 = { error: e_2_1 };
+                return [3 /*break*/, 12];
+            case 7:
+                _h.trys.push([7, , 10, 11]);
+                if (!(!_a && !_d && (_e = _b.return))) return [3 /*break*/, 9];
+                return [4 /*yield*/, _e.call(_b)];
+            case 8:
+                _h.sent();
+                _h.label = 9;
+            case 9: return [3 /*break*/, 11];
+            case 10:
+                if (e_2) throw e_2.error;
+                return [7 /*endfinally*/];
+            case 11: return [7 /*endfinally*/];
+            case 12:
+                elapsed = Date.now() - startTime;
+                console.log("\nProcessed ".concat(count, " identities in ").concat(elapsed, "ms"));
+                console.log("Started processing immediately, didn't wait for all data!");
+                return [2 /*return*/];
+        }
+    });
+}); };
+var compareGeneratorVsRegularPagination = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var apiConfig, api, regularStart, allGroups, regularTime, generatorStart, genCount, firstItemTime, _a, _b, _c, group, e_3_1, generatorTime;
+    var _d, e_3, _e, _f;
+    return __generator(this, function (_g) {
+        switch (_g.label) {
+            case 0:
+                console.log("\n=== Comparing Generator vs Regular Pagination ===");
+                apiConfig = new sailpoint_api_client_1.Configuration();
+                api = new sailpoint_api_client_1.GovernanceGroupsBetaApi(apiConfig);
+                // Regular pagination - waits for all data
+                console.log("\n1. Regular Pagination (waits for all data):");
+                regularStart = Date.now();
+                return [4 /*yield*/, sailpoint_api_client_1.Paginator.paginate(api, api.listWorkgroups, { limit: 50 }, 10)];
+            case 1:
+                allGroups = _g.sent();
+                regularTime = Date.now() - regularStart;
+                console.log("   Fetched all ".concat(allGroups.data.length, " groups in ").concat(regularTime, "ms"));
+                console.log("   Now processing them...");
+                allGroups.data.forEach(function (group, i) {
+                    if (i < 3)
+                        console.log("   - ".concat(group.name));
+                });
+                // Generator pagination - processes as data arrives
+                console.log("\n2. Generator Pagination (processes immediately):");
+                generatorStart = Date.now();
+                genCount = 0;
+                firstItemTime = 0;
+                _g.label = 2;
+            case 2:
+                _g.trys.push([2, 7, 8, 13]);
+                _a = true, _b = __asyncValues(sailpoint_api_client_1.Paginator.paginateGenerator(api, api.listWorkgroups, { limit: 50 }, 10));
+                _g.label = 3;
+            case 3: return [4 /*yield*/, _b.next()];
+            case 4:
+                if (!(_c = _g.sent(), _d = _c.done, !_d)) return [3 /*break*/, 6];
+                _f = _c.value;
+                _a = false;
+                group = _f;
+                genCount++;
+                if (genCount === 1) {
+                    firstItemTime = Date.now() - generatorStart;
+                    console.log("   First item available after ".concat(firstItemTime, "ms!"));
+                }
+                if (genCount <= 3) {
+                    console.log("   - ".concat(group.name));
+                }
+                _g.label = 5;
+            case 5:
+                _a = true;
+                return [3 /*break*/, 3];
+            case 6: return [3 /*break*/, 13];
+            case 7:
+                e_3_1 = _g.sent();
+                e_3 = { error: e_3_1 };
+                return [3 /*break*/, 13];
+            case 8:
+                _g.trys.push([8, , 11, 12]);
+                if (!(!_a && !_d && (_e = _b.return))) return [3 /*break*/, 10];
+                return [4 /*yield*/, _e.call(_b)];
+            case 9:
+                _g.sent();
+                _g.label = 10;
+            case 10: return [3 /*break*/, 12];
+            case 11:
+                if (e_3) throw e_3.error;
+                return [7 /*endfinally*/];
+            case 12: return [7 /*endfinally*/];
+            case 13:
+                generatorTime = Date.now() - generatorStart;
+                console.log("   Processed ".concat(genCount, " groups in ").concat(generatorTime, "ms"));
+                console.log("   Started processing ".concat(regularTime - firstItemTime, "ms earlier than regular pagination!"));
+                return [2 /*return*/];
+        }
+    });
+}); };
+// Run examples
+// listEntitlements()
+// getPaginatedAccountsWithGenerator()
+// searchWithGenerator()
+compareGeneratorVsRegularPagination();
--- sdk-output/configuration.ts
+++ sdk-output/configuration.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
+import { IAxiosRetryConfig } from "axios-retry";
+import FormData from "form-data";
+import * as fs from "fs";
+import * as yaml from "js-yaml";
 import * as os from "os";
 import * as path from "path";
-import * as yaml from "js-yaml";
-import * as fs from "fs";
-import FormData from "form-data";
-import { IAxiosRetryConfig } from "axios-retry";
 import { ProxyAgent } from "proxy-agent";
 
 export interface ConfigurationParameters {
   baseurl?: string;
+  nermBaseurl?: string;
   clientId?: string;
   clientSecret?: string;
   accessToken?: string;
@@ -29,6 +30,7 @@ export interface Configuration {
 
 export interface Environment {
   baseurl: string;
+  nermBaseurl: string;
   pat: Pat;
   tenanturl: string;
 }
@@ -110,6 +112,15 @@ export class Configuration {
    * @memberof Configuration
    */
   basePath?: string;
+
+  /**
+   * override base path for NERM
+   *
+   * @type {string}
+   * @memberof Configuration
+   */
+  nermBasePath?: string;
+
   /**
    * base options for axios calls
    *
@@ -147,6 +158,7 @@ export class Configuration {
 
     this.accessToken = param.accessToken;
     this.basePath = param.baseurl;
+    this.nermBasePath = param.nermBaseurl;
     this.tokenUrl = param.tokenUrl;
     this.clientId = param.clientId;
     this.clientSecret = param.clientSecret;
@@ -171,6 +183,7 @@ export class Configuration {
       ) as Configuration;
       if (doc.authtype && doc.authtype.toLowerCase() === "pat") {
         config.baseurl = doc.environments[doc.activeenvironment].baseurl;
+        config.nermBaseurl = doc.environments[doc.activeenvironment].nermBaseurl;
         config.clientId = doc.environments[doc.activeenvironment].pat.clientid;
         config.clientSecret =
           doc.environments[doc.activeenvironment].pat.clientsecret;
@@ -192,6 +205,7 @@ export class Configuration {
       config.clientId = jsonData.ClientId;
       config.clientSecret = jsonData.ClientSecret;
       config.tokenUrl = config.baseurl + "/oauth/token";
+      config.nermBaseurl = jsonData.NERMBaseURL;
     } catch (error) {
       console.log("unable to find config file in local directory");
     }
@@ -208,6 +222,10 @@ export class Configuration {
       : "";
     config.clientSecret = process.env["SAIL_CLIENT_SECRET"]
       ? process.env["SAIL_CLIENT_SECRET"]
+      : "";
+
+    config.nermBaseurl = process.env["SAIL_NERM_BASE_URL"]
+      ? process.env["SAIL_NERM_BASE_URL"]
       : "";
 
     config.tokenUrl = config.baseurl + "/oauth/token";
