13c4ded75f274e03b4e121b600fd8ccf7002f80f
luke-hagar-sp <193985956+luke-hagar-sp@users.noreply.github.com>
2026-03-26 08:28:26 +0000

    Bump version to v1.8.7

--- sdk-output/nermv2025/common.ts
+++ sdk-output/nermv2025/common.ts
@@ -146,7 +146,7 @@ export const toPathString = function (url: URL) {
 export const createRequestFunction = function (axiosArgs: RequestArgs, globalAxios: AxiosInstance, BASE_PATH: string, configuration?: Configuration) {
     return <T = unknown, R = AxiosResponse<T>>(axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
         axiosRetry(axios, configuration.retriesConfig)
-        let userAgent = `SailPoint-SDK-TypeScript/1.8.6`;
+        let userAgent = `SailPoint-SDK-TypeScript/1.8.7`;
         if (configuration?.consumerIdentifier && configuration?.consumerVersion) {
             userAgent += ` (${configuration.consumerIdentifier}/${configuration.consumerVersion})`;
         }
@@ -154,7 +154,7 @@ export const createRequestFunction = function (axiosArgs: RequestArgs, globalAxi
         const headers = {
             ...{'Accept': 'application/json'},
             ...axiosArgs.axiosOptions.headers,
-            ...{'X-SailPoint-SDK':'typescript-1.8.6'},
+            ...{'X-SailPoint-SDK':'typescript-1.8.7'},
             ...{'User-Agent': userAgent},
         }
 
